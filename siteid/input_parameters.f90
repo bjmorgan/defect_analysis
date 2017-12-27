fc0d638e59709403954bd89c34de9322c794c84a
Benjamin Morgan <benmorgan2@gmail.com>
2017-12-27 18:38:34 +0000

    Runtime parameters are now stored in an input_paramter_set class
    This class includes a params%read_from_file( filename ) method
    to read in the input parameters.

---
+++ siteid/input_parameters.f90
@@ -0,0 +1,48 @@
+module input_parameters
+
+    implicit none
+
+    type input_parameter_set
+        character(len=30) :: posfile, cellfile, inptfile, tetfile, octfile
+        integer :: nconfigs, nspec, lattice_spec, mobile_spec
+        integer, allocatable, dimension(:) :: nsp
+        integer :: ntet, noct
+        logical :: variable_cell
+        double precision, dimension(3) :: boxlen
+        double precision, dimension(3,3) :: h
+    contains
+        procedure :: read_from_file
+    end type input_parameter_set
+
+    contains
+
+    subroutine read_from_file( params, inptfile )
+        class(input_parameter_set) :: params
+        character(len=30), intent(in) :: inptfile
+        integer :: fin, i
+
+        open( file=inptfile, status='old', newunit=fin )
+        read( fin, * ) params%posfile
+        read( fin, * ) params%cellfile
+        read( fin, * ) params%tetfile
+        read( fin, * ) params%octfile
+        read( fin, * ) params%nconfigs
+        read( fin, * ) params%nspec
+        allocate (params%nsp(params%nspec))
+        do i=1, params%nspec
+            read(fin,*) params%nsp(i)
+        enddo
+        read( fin, * ) params%lattice_spec
+        read( fin, * ) params%mobile_spec
+        read( fin, * ) params%ntet
+        read( fin, * ) params%noct
+        read( fin, * ) params%variable_cell
+        read( fin, * ) params%boxlen(:)
+        read( fin, * ) params%h(:,:)
+        close(fin)
+    end subroutine read_from_file
+
+end module input_parameters
+
+        
+
--- siteid/main.f90
+++ siteid/main.f90
@@ -4,6 +4,7 @@ use atoms
 use tetrahedra
 use octahedra
 use cell
+use input_parameters
  
 implicit none
 
@@ -11,18 +12,18 @@ type species
     type (atom), dimension(:), allocatable :: ion
 end type species  
 
+type (input_parameter_set) :: params
 type (species), dimension(:), allocatable :: spec
 class (tetrahedron), dimension(:), allocatable :: tetra
 class (octahedron), dimension(:), allocatable :: octa
 
-character(len=30) :: posfile, cellfile, inptfile, tetfile, octfile, poly_out, atoms_out, npolyocc
+character(len=30) :: inptfile, poly_out, atoms_out, npolyocc
 integer :: i, j, polyswitch
-integer :: nconfigs, nstep, thispoly, nspec, lattice_spec, mobile_spec
+integer :: nstep, thispoly
 integer :: natomsout ! number of mobile atoms 
-integer, allocatable, dimension(:) :: nsp, polylist, sitelist
+integer, allocatable, dimension(:) :: polylist, sitelist
 character(len=30) :: fmtout, fmtout2
 integer :: fin, fout1, fout2, fout3, fcell
-logical :: variable_cell
 
 interface
 
@@ -38,64 +39,53 @@ poly_out  = 'sites_atoms.dat'
 atoms_out = 'atoms_sites.dat'
 npolyocc  = 'npolyocc.dat'
 
-open( file=inptfile, status='old', newunit=fin)
-read( fin, * ) posfile
-read( fin, * ) cellfile
-read( fin, * ) tetfile
-read( fin, * ) octfile
-read( fin, * ) nconfigs
-read( fin, * ) nspec
-allocate (nsp(nspec))
-allocate (spec(nspec))
-do i=1, nspec
-    read(fin,*) nsp(i)
-    allocate (spec(i)%ion(nsp(i)))
-    forall (j=1:nsp(i)) spec(i)%ion(j)%id = j
-enddo
-read( fin, * ) lattice_spec
-read( fin, * ) mobile_spec
-read( fin, * ) ntet
-read( fin, * ) noct
-read( fin, * ) variable_cell
-read( fin, * ) boxlen(:)
-read( fin, * ) h(:,:)
-close(fin)
+call params%read_from_file( inptfile )
 
-if ( .not. variable_cell ) then
+allocate (spec(params%nspec))
+do i=1, params%nspec
+    allocate (spec(i)%ion(params%nsp(i)))
+    forall (j=1:params%nsp(i)) spec(i)%ion(j)%id = j
+enddo
+
+if ( .not. params%variable_cell ) then
+    boxlen = params%boxlen
+    h = params%h
     halfboxlen = boxlen / 2.0
     cboxlen = diagonal( h ) * boxlen
     halfcboxlen = cboxlen / 2.0
 endif
 
-call setup_tet(tetra, ntet)
-call setup_oct(octa, noct)
+call setup_tet( tetra, params%ntet )
+call setup_oct( octa, params%noct )
 
-natomsout = nsp( mobile_spec ) 
+natomsout = params%nsp( params%mobile_spec ) 
 
 allocate( polylist( natomsout ) )
-allocate( sitelist( ntet+noct ) )
+allocate( sitelist( params%ntet+params%noct ) )
  
 ! read in ids for tetrahedra vertices
-open(file=tetfile, status='old', newunit=fin)
-do i=1, ntet 
+! TODO this should go in the tetrahedra.f90 file
+open(file=params%tetfile, status='old', newunit=fin)
+do i=1, params%ntet 
     read(fin,*) tetra(i)%vertex_ids
 enddo
 close(fin)
 
 ! read in ids for octahedra vertices
-open(file=octfile, status='old', newunit=fin)
-do i=1, noct
+! TODO this should go in the octahedra.f90 file
+open(file=params%octfile, status='old', newunit=fin)
+do i=1, params%noct
     read(fin,*) octa(i)%vertex_ids
 enddo
 close(fin)
 
-open( file = posfile,   status='old', newunit=fin )
-open( file = cellfile,  status='old', newunit=fcell )
+open( file = params%posfile,   status='old', newunit=fin )
+open( file = params%cellfile,  status='old', newunit=fcell )
 open( file = npolyocc,  newunit=fout1 )
 open( file = atoms_out, newunit=fout2 )
 open( file = poly_out,  newunit=fout3 )
 
-do concurrent (i=1:nspec)
+do concurrent (i=1:params%nspec)
     spec(i)%ion%polyid = 0
     spec(i)%ion%inoct = .false.
     spec(i)%ion%intet = .false.
@@ -103,23 +93,25 @@ do concurrent (i=1:nspec)
     spec(i)%ion%prev_inoct = .false.
 end do
 
-do nstep=1, nconfigs
+do nstep=1, params%nconfigs
 
     tetra%occnum = 0
     tetra%occupied = .false.
     octa%occnum = 0
     octa%occupied = .false.
 
-    do j=1, nsp( mobile_spec )
-        spec(mobile_spec)%ion(j)%previous_polyid = spec(mobile_spec)%ion(j)%polyid
-        spec(mobile_spec)%ion(j)%prev_inoct = spec(mobile_spec)%ion(j)%inoct
-        spec(mobile_spec)%ion(j)%prev_intet = spec(mobile_spec)%ion(j)%intet
-        spec(mobile_spec)%ion(j)%polyid = 0
-        spec(mobile_spec)%ion(j)%inoct  = .false.
-        spec(mobile_spec)%ion(j)%intet  = .false.
+    do j=1, params%nsp( params%mobile_spec )
+        associate( this_ion => spec(params%mobile_spec)%ion(j) )
+            this_ion%previous_polyid = this_ion%polyid
+            this_ion%prev_inoct = this_ion%inoct
+            this_ion%prev_intet = this_ion%intet
+            this_ion%polyid = 0
+            this_ion%inoct  = .false.
+            this_ion%intet  = .false.
+        end associate
     end do
 
-    if ( variable_cell ) then
+    if ( params%variable_cell ) then
         read( fcell, * ) h(:,:) ! cell unit vectors are rows in h
         read( fcell, * ) boxlen(:) 
         halfboxlen = boxlen / 2.0
@@ -127,52 +119,52 @@ do nstep=1, nconfigs
         halfcboxlen = cboxlen / 2.0
     endif
  
-    do i=1, nspec
-        do j=1, nsp(i)
+    do i=1, params%nspec
+        do j=1, params%nsp(i)
             associate( this_ion => spec(i)%ion(j) )
                 read(fin,*) this_ion%r(1:3) 
-!                 if( spec(i) == mobile_spec .and. j==329 ) write(6,*) this_ion%r        
                 this_ion%r = move_inside_cell( this_ion%r ) ! apply periodic boundary conditions
-!                 if( spec(i) == mobile_spec .and. j==329 ) write(6,*) this_ion%r 
             end associate
         enddo
     enddo
   
-    do thispoly = 1, ntet ! construct tetrahedra
-        call tetra(thispoly)%set_vertices_from_ids( spec( lattice_spec )%ion )
+    do thispoly = 1, params%ntet ! construct tetrahedra
+        call tetra(thispoly)%set_vertices_from_ids( spec( params%lattice_spec )%ion )
         call tetra(thispoly)%enforce_pbc
         call tetra(thispoly)%assign_faces
     end do
 
-    do thispoly = 1, noct ! construct octahedra
-        call octa(thispoly)%set_vertices_from_ids( spec( lattice_spec )%ion )
+    do thispoly = 1, params%noct ! construct octahedra
+        call octa(thispoly)%set_vertices_from_ids( spec( params%lattice_spec )%ion )
         call octa(thispoly)%enforce_pbc
         call octa(thispoly)%assign_faces
     end do
 
-    ionloop: do j=1, nsp( mobile_spec )
-        ! test whether ions are in the same polyhedra as the previous step
-        if ( spec( mobile_spec )%ion(j)%prev_intet ) then
-            call tetra( spec( mobile_spec )%ion(j)%previous_polyid )%occupied_by( spec( mobile_spec )%ion(j) )
-        else if ( spec( mobile_spec )%ion(j)%prev_inoct ) then
-            call  octa( spec( mobile_spec )%ion(j)%previous_polyid )%occupied_by( spec( mobile_spec )%ion(j) )
-        end if
-        if ( spec(mobile_spec)%ion(j)%intet .or. spec(mobile_spec)%ion(j)%inoct ) cycle ionloop
-        ! ion has moved. search over remaining tetrahedra
-        tetloop: do thispoly = 1, ntet
-            if ( spec( mobile_spec )%ion(j)%prev_intet .and. thispoly .eq. spec( mobile_spec)%ion(j)%previous_polyid ) cycle tetloop
-            call tetra( thispoly )%occupied_by( spec(mobile_spec)%ion(j) )
-            if ( spec(mobile_spec)%ion(j)%intet ) cycle ionloop
-        end do tetloop
-        ! search over remaining octahedra
-        octloop: do thispoly = 1, noct
-            if ( spec( mobile_spec )%ion(j)%prev_inoct .and. thispoly .eq. spec( mobile_spec)%ion(j)%previous_polyid ) cycle octloop
-            call octa( thispoly )%occupied_by( spec(mobile_spec)%ion(j) )
-            if ( spec(mobile_spec)%ion(j)%inoct ) cycle ionloop
-        end do octloop
-        ! if we reach here without cycling ionloop, this ion has not been located in any polyhedron
-        write(6,*) 'Ion ', j,' not in any polyhedra, at ', spec(mobile_spec)%ion(j)%r
-        stop
+    ionloop: do j=1, params%nsp( params%mobile_spec )
+        associate( this_ion => spec( params%mobile_spec )%ion(j) )
+            ! test whether ions are in the same polyhedra as the previous step
+            if ( this_ion%prev_intet ) then
+                call tetra( this_ion%previous_polyid )%occupied_by( this_ion )
+            else if ( this_ion%prev_inoct ) then
+                call  octa( this_ion%previous_polyid )%occupied_by( this_ion )
+            end if
+            if ( this_ion%intet .or. this_ion%inoct ) cycle ionloop
+            ! ion has moved. search over remaining tetrahedra
+            tetloop: do thispoly = 1, params%ntet
+                if ( this_ion%prev_intet .and. thispoly .eq. this_ion%previous_polyid ) cycle tetloop
+                call tetra( thispoly )%occupied_by( this_ion )
+                if ( this_ion%intet ) cycle ionloop
+            end do tetloop
+            ! search over remaining octahedra
+            octloop: do thispoly = 1, params%noct
+                if ( this_ion%prev_inoct .and. thispoly .eq. this_ion%previous_polyid ) cycle octloop
+                call octa( thispoly )%occupied_by( this_ion )
+                if ( this_ion%inoct ) cycle ionloop
+            end do octloop
+            ! if we reach here without cycling ionloop, this ion has not been located in any polyhedron
+            write(6,*) 'Ion ', j,' not in any polyhedra, at ', this_ion%r
+            stop
+        end associate
     end do ionloop
 
     write(6,*) "step ", nstep, count(tetra%occupied), count(octa%occupied)
@@ -180,8 +172,8 @@ do nstep=1, nconfigs
 
     natomsout = 0
     polylist = 0
-    do j=1, nsp( mobile_spec )
-        associate( this_ion => spec(mobile_spec)%ion(j) )
+    do j=1, params%nsp( params%mobile_spec )
+        associate( this_ion => spec(params%mobile_spec)%ion(j) )
             natomsout = natomsout + 1
             if ( this_ion%inoct ) then
                 polyswitch = -1
@@ -192,11 +184,11 @@ do nstep=1, nconfigs
         end associate
     enddo
 
-    sitelist( 1:ntet ) = tetra%occnum
-    sitelist( ntet+1:ntet+noct ) = octa%occnum
+    sitelist( 1:params%ntet ) = tetra%occnum
+    sitelist( params%ntet+1:params%ntet+params%noct ) = octa%occnum
 
     write( fmtout,  '(A4,I5,A7)') "(I5,", natomsout, "(I6,X))" ! internal write to define output formatting
-    write( fmtout2, '(A4,I5,A7)') "(I5,", ntet+noct, "(I6,X))" ! internal write to define output formatting
+    write( fmtout2, '(A4,I5,A7)') "(I5,", params%ntet+params%noct, "(I6,X))" ! internal write to define output formatting
     write( fout2, fmtout )  nstep, polylist(:)
     write( fout3, fmtout2 ) nstep, sitelist(:)
 
